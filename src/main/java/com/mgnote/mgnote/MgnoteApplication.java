2379b2de658b32d34f9b8654583ef69c988dc116
ZCH273711 <chenghang_zheng@163.com>
2020-12-10 10:13:08 +0800

    初始化

---
+++ src/main/java/com/mgnote/mgnote/MgnoteApplication.java
@@ -0,0 +1,13 @@
+package com.mgnote.mgnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgnoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MgnoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mgnote/mgnote/MgnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgnote.mgnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgnoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
